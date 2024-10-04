ff8e52eba87ee4cb251997463d51ee1b817674c0
deepanshu-jft <deepanshu.kaushik@jellyfishtechnologies.com>
2024-10-04 12:26:04 +0530

    undo scroll changes

--- script.js
+++ script.js
@@ -165,7 +165,6 @@ function initializeIntroPage(CompanyBotName) {
   botIntroMinimizeImg.addEventListener('click', () => {
     botIntroPage.style.display = 'none';
     botButton.style.display = 'flex';
-    document.body.classList.remove('no-scroll');
   });
   botIntroMinimize.appendChild(botIntroMinimizeImg);
   botIntroPage.appendChild(botIntroMinimize);
@@ -264,7 +263,6 @@ function initializeBot() {
     minimizeBtn.addEventListener('click', () => {
       botContainer.style.display = 'none';
       botButton.removeAttribute('style');
-      document.body.classList.remove('no-scroll');
     });
   });
 
@@ -278,7 +276,6 @@ function initializeBot() {
   botButton.addEventListener('click', () => {
     botButton.style.display = 'none';
     document.querySelector('.chatbot-intro-page').style.display = 'flex';
-    document.body.classList.add('no-scroll');
   });
 
   document
